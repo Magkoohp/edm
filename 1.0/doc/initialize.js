cc10c4a34b061fe33d4be4b4e559bc4f2e80a42c
root <root@ip-172-31-25-69.eu-west-1.compute.internal>
2023-05-25 15:40:23 +0200

    initialize with existing data

---
+++ 1.0/doc/initialize.js
@@ -0,0 +1,58 @@
+    $("#toc").toc({content: "#mybody", headings: "h3,h4,h5"});
+    anchors.add("h3,h4,h5");
+
+    $toc_elem = $('#h2_toc');
+    $return_top_elem = $('#return-to-top');
+ // ===== Scroll to Top ==== 
+    $(window).scroll(function() {
+        if ($(this).scrollTop() >= $toc_elem.offset().top ) {        // If page is scrolled more than 50px
+        	$return_top_elem.fadeIn(200);    // Fade in the arrow
+        } else {
+        	$return_top_elem.fadeOut(200);   // Else fade out the arrow
+        }
+    });
+    $return_top_elem.click(function() {      // When arrow is clicked
+        $('body,html').animate({
+        	
+            scrollTop : $toc_elem.offset().top                       // Scroll to top of body
+        }, 500);
+    });
+    
+	$(document).ready(function() {
+	    $cellsDataType = $('.generatedTable td:nth-child(4)');
+	    $cellsDataType.each(function( index ) {
+	    	  
+	    	  celltext = $( this ).text();
+	    	  console.log(celltext);
+	    	  if (!String.prototype.startsWith) {
+	    		    String.prototype.startsWith = function(searchString, position){
+	    		      position = position || 0;
+	    		      return this.substr(position, searchString.length) === searchString;
+	    		  };
+	    	  }
+	    	  var cellprefix = celltext.split(":")[0];
+	    	  var cellvalue = celltext.split(":")[1];
+	    	  if(!(url_array[cellprefix] == null)) {
+	    		  $( this ).html("<a href='" + url_array[cellprefix] + "#"+cellvalue+"'>"+celltext+"</a>");
+	    	  }
+	    	});
+	    
+	    $('.generatedTable').DataTable( {
+	    	"paging":   false,
+	    	"info":     false,
+	    	"searching": false,
+	        responsive: {
+	            details: {
+	                display: $.fn.dataTable.Responsive.display.modal( {
+	                    header: function ( row ) {
+	                        var data = row.data();
+	                        return 'Details for '+data[0]+' '+data[1];
+	                    }
+	                } ),
+	                renderer: $.fn.dataTable.Responsive.renderer.tableAll( {
+	                    tableClass: 'table'
+	                } )
+	            }
+	        }
+	    } );
+	} );
